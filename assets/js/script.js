96cf022b410724509f4f041c9088494446440927
OnSake <julesmuller06@gmail.com>
2024-04-26 22:16:28 +0200

    amélioration js button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
-var printImg = document.getElementById("photo-image");
+const photoImage = document.getElementById("photo-image"),
+  imageButton = document.getElementById("image-launch-button");
+retakeButton = document.getElementById("retake-image-button");
 
-function printImage() {
-  printImg.print();
+function launchImage() {
+  console.log("launchImage lancé");
+  photoImage.classList.remove("cache");
+  imageButton.classList.add("cache");
+  retakeButton.classList.remove("retake_image");
+}
+
+function retakeImage() {
+  console.log("retakeImage lancé");
+  photoImage.classList.add("cache");
+  imageButton.classList.remove("cache");
+  retakeButton.classList.add("retake_image");
 }
